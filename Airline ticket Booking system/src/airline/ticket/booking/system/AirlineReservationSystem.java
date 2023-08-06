b10ee4836b9c26e3fd212f0c8c2848f723fdfcec
Sineha <sinehakatariya@gmail.com>
2023-08-06 15:21:14 -0700

    four_semester_project

---
+++ Airline ticket Booking system/src/airline/ticket/booking/system/AirlineReservationSystem.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package airline.ticket.booking.system;
+
+import java.awt.Color;
+import java.sql.*;
+
+/**
+ *
+ * @author 92336
+ */
+public class AirlineReservationSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Startupform entry1=new Startupform();
+      entry1.getContentPane().setBackground(Color.WHITE);
+     entry1.show();
+        
+    }
+    
+}
